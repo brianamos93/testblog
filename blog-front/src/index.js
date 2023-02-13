e71009f19215167d264145885112b8b36ac3bef7
Brian <brianizzo93@gmail.com>
2023-02-13 15:48:34 +0900

    removed axios from index.js

--- blog-front/src/index.js
+++ blog-front/src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import axios from 'axios'
 
-
-axios.get('http://localhost:3001/posts').then(response => {
-  const posts = response.data
   ReactDOM.createRoot(document.getElementById('root')).render(
-    <App posts={posts} />
+    <App />
   )
-})
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
